5b35347aff266c1bad7109370c12f0f1f75bf8aa
Vyacheslav Strizhius <v.v.strizhius@gmail.com>
2025-02-18 16:08:32 +0300

    changed gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -1,1 +1,3 @@
-console.log(diff);
+const help = '\n  Usage: gendiff [options] <filepath1> <filepath2>\n\n  Compares two configuration files and show a difference.\n\n  Options:\n    -V, --version        output the version number\n    -h, --help           output usage information\n    -f, --format <type>  output format';
+
+console.log(help);
