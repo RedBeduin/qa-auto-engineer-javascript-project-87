cee6b9dd6f57438b826250f080c42404bd70e8a0
Vyacheslav Strizhius <v.v.strizhius@gmail.com>
2025-02-22 00:01:54 +0300

    changed gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -1,3 +1,18 @@
-const help = '\n  Usage: gendiff [options] <filepath1> <filepath2>\n\n  Compares two configuration files and show a difference.\n\n  Options:\n    -V, --version        output the version number\n    -h, --help           output usage information\n    -f, --format <type>  output format';
+#!/usr/bin/env node
+const { program } = require('commander');
 
-console.log(help);
+const help_text = '\n  Usage: gendiff [options] <filepath1> <filepath2>\n\n  Compares two configuration files and show a difference.\n\n  Options:\n    -V, --version        output the version number\n    -h, --help           output usage information\n    -f, --format <type>  output format';
+
+const command_help = (help_message) => 
+{
+  console.log(help_text);
+};
+program
+  .option('-h, --help'); // описание можно скопировать из переменной help_text
+
+program.parse(process.argv);        
+
+const options = program.opts();
+const { help_message } = options; 
+
+command_help(help_message);
