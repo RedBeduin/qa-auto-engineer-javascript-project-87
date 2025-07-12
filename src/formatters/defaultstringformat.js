4144372871d9757e21890064b88e5fe4af02d21e
Vyacheslav Strizhius <v.v.strizhius@gmail.com>
2025-07-12 17:37:33 +0300

    fixed display of the result of the gendiffcommand execution in "src/formatters/defaultstringformat.js"

--- src/formatters/defaultstringformat.js
+++ src/formatters/defaultstringformat.js
@@ -3,19 +3,19 @@ const defaultstringformat = (tree) => {
   for(const element of tree) {
     if (element.action === 'added')
     {
-      result = `${result}\n + ${element.key}: ${element.obj}`;
+      result = `${result}\n  + ${element.key}: ${element.obj}`;
     }
     else if (element.action === 'removed')
     {
-      result = `${result}\n - ${element.key}: ${element.obj}`;
+      result = `${result}\n  - ${element.key}: ${element.obj}`;
     }
     else if (element.action === 'updated')
     {
-      result = `${result}\n - ${element.key}: ${element.objFirst}\n + ${element.key}: ${element.objSecond}`;
+      result = `${result}\n  - ${element.key}: ${element.objFirst}\n  + ${element.key}: ${element.objSecond}`;
     }
     else if (element.action === 'mapped')
     {
-      result = `${result}\n   ${element.key}: ${element.obj}`;
+      result = `${result}\n    ${element.key}: ${element.obj}`;
     }
   };
   return `{${result}\n}`;    
