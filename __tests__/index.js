45f524beea29aa00da8fecebbf17975c16b33664
Vyacheslav Strizhius <v.v.strizhius@gmail.com>
2025-07-28 16:03:25 +0300

    fixed filepaths(saved into constants) in __tests__/index.js

--- __tests__/index.js
+++ __tests__/index.js
@@ -4,13 +4,13 @@ import gendiff from '../src'
 const getFixturePathNew = path => new URL(path, import.meta.url)
 const readFile = filename => fs.readFileSync(getFixturePathNew(filename), 'utf-8')
 
-const pathJsonFirst = './__tests__/__fixtures__/ordinary_test_1.json'
-const pathJsonSecond = './__tests__/__fixtures__/ordinary_test_2.json'
-const yamlFirst = './__tests__/__fixtures__/yaml_test_1.yml'
-const yamlSecond = './__tests__/__fixtures__/yaml_test_2.yml'
-const pathResult = './__tests__/__fixtures__/raw_result.txt'
-const pathResultPlain = './__tests__/__fixtures__/plain_result.txt'
-const pathResultJson = './__tests__/__fixtures__/json_result.txt'
+const pathJsonFirst = './__fixtures__/ordinary_test_1.json'
+const pathJsonSecond = './__fixtures__/ordinary_test_2.json'
+const yamlFirst = './__fixtures__/yaml_test_1.yml'
+const yamlSecond = './__fixtures__/yaml_test_2.yml'
+const pathResult = './__fixtures__/raw_result.txt'
+const pathResultPlain = './__fixtures__/plain_result.txt'
+const pathResultJson = './__fixtures__/json_result.txt'
 
 test.each([
   { fileFirst: pathJsonFirst, fileSecond: pathJsonSecond, result: readFile(pathResult) },
