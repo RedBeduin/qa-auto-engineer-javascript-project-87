f32ccbbd7bccda1ee5b44b71fe180fc8d89f2a56
Vyacheslav Strizhius <v.v.strizhius@gmail.com>
2025-07-31 23:50:23 +0300

    changed __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,7 @@
 import * as fs from 'node:fs'
 import gendiff from '../src'
-import path from 'node:path'
 
-const getFixturePath = fixtureFileName => path.resolve(process.cwd(), fixtureFileName)
+const getFixturePath = fixtureFileName => `../${new URL(fixtureFileName, import.meta.url)}`
 const readFile = fileName => fs.readFileSync(getFixturePath(fileName), 'utf-8')
 
 const tests = ['json', 'yml']
