d60064d4d4c63655d8b6621d0de91d16053a4523
Vyacheslav Strizhius <v.v.strizhius@gmail.com>
2025-08-01 22:09:30 +0300

    changed src/index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import path from 'node:path'
 const gendiff = (filepath1, filepath2, format) => {
   const readFile = (filepath) => {
     filepath = path.resolve(process.cwd(), filepath)
-    const data = parse(fs.readFileSync(filepath, 'utf-8').toString())
+    const data = fs.readFileSync(filepath, 'utf-8').toString()
     const format = path.extname(filepath).slice(1).toLowerCase()
-    return { data, format }
+    return parse({ data, format })
   }
   const tree = generateDifferences(readFile(filepath1), readFile(filepath2))
   return formatTree(tree, format)
