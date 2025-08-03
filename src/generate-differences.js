6c19f3a55fb44bcdbdb2f6e60f55ee793c66c086
Vyacheslav Strizhius <v.v.strizhius@gmail.com>
2025-08-03 23:06:09 +0300

    changed src/generate-differences.js

--- src/generate-differences.js
+++ src/generate-differences.js
@@ -7,20 +7,16 @@ const generateDifferences = (data1, data2) => {
 
   return _.sortBy(keys.map((key) => {
     if (data1[key] !== data2[key]
-      && _.includes(key)
       && !_.includes(data1Key, key)) {
       return { key, data: data2[key], action: 'added' }
     }
 
     if (data1[key] !== data2[key]
-      && _.includes(key)
       && !_.includes(data2Key, key)) {
       return { key, data: data1[key], action: 'removed' }
     }
 
-    if (data1[key] !== data2[key]
-      && _.includes(data1Key)
-      && _.includes(data2Key)) {
+    if (data1[key] !== data2[key]) {
       return { key, data1: data1[key], data2: data2[key], action: 'updated' }
     }
 
